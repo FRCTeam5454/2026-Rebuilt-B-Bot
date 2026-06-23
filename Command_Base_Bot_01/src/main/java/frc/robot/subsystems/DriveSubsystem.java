df155407efd626e6d3e057194c5e71d0e822ebe5
Karl Kay <kkay99@gmail.com>
2026-06-22 22:50:39 -0500

    1st Drive Code from Dev Projects

---
+++ Command_Base_Bot_01/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,57 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+
+
+public class DriveSubsystem extends SubsystemBase {
+  private final WPI_VictorSPX m_leftLeader = new WPI_VictorSPX(Constants.kLeftMotor1Port);
+  private final WPI_VictorSPX m_leftFollower = new WPI_VictorSPX(Constants.kLeftMotor2Port);
+  private final WPI_VictorSPX m_rightLeader = new WPI_VictorSPX(Constants.kRightMotor1Port);
+  private final WPI_VictorSPX m_rightFollower = new WPI_VictorSPX(Constants.kRightMotor2Port);
+  private final DifferentialDrive m_drive;
+  
+  /** Creates a new DriveSubsystem. */
+  public DriveSubsystem() {
+    // The motors on the left side of the drive.
+    m_leftFollower.follow(m_leftLeader);
+    m_rightFollower.follow(m_rightLeader);
+  // The motors on the right side of the drive.
+    m_rightLeader.setInverted(true);
+    m_rightFollower.setInverted(true);
+    m_drive = new DifferentialDrive(m_leftLeader, m_rightLeader);
+  }
+
+  public void driveRobot(double LeftSpeed, double RightSpeed){
+  m_drive.tankDrive(LeftSpeed, RightSpeed);
+  }
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public Command exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
