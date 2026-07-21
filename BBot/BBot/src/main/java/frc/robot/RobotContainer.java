d349756327ed7d9478c6fced102e391c169daa8f
Karl Kay <kkay99@gmail.com>
2026-07-20 19:40:17 -0500

    ShooterUpdates

--- BBot/BBot/src/main/java/frc/robot/RobotContainer.java
+++ BBot/BBot/src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,9 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.ShooterCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.ShootingSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -20,7 +22,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final ShootingSubsystem m_shootingSubsystem = new ShootingSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -42,13 +44,11 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+    Command shootCommand = new ShooterCommand(m_shootingSubsystem, Constants.ShooterConstants.ShooterSpeed);
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    m_driverController.rightTrigger().whileTrue(shootCommand);
   }
 
   /**
@@ -58,6 +58,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return null;
   }
 }
--- BBot/BBot/src/main/java/frc/robot/subsystems/ShootingSubsystem.java
+++ BBot/BBot/src/main/java/frc/robot/subsystems/ShootingSubsystem.java
@@ -8,35 +8,26 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.utilities.ObsidianCANSparkMax;
 public class ShootingSubsystem extends SubsystemBase {
+   final ObsidianCANSparkMax m_rightLeader = new ObsidianCANSparkMax(Constants.ShooterConstants.LowerShooterMotorPort, ObsidianCANSparkMax.MotorType.kBrushless, false);
+   final ObsidianCANSparkMax m_rightFollower = new ObsidianCANSparkMax(Constants.ShooterConstants.UpperShooterMotorPort, ObsidianCANSparkMax.MotorType.kBrushless, false);
+   final ObsidianCANSparkMax m_leftFollower = new ObsidianCANSparkMax(Constants.ShooterConstants.AnotherShooterMotorPort, ObsidianCANSparkMax.MotorType.kBrushless, false);
+  
   /** Creates a new ShootingSubsystem. */
-  public ShootingSubsystem() {}
-
-  /*
-   * Shooting command factory method.
-   *
-   * @return a command
-   */
-  public Command exampleMethodCommand() {
-      private final ObsidianCANSparkMax m_rightLeader = new ObsidianCANSparkMax(Constants.LowerShooterMotorPort);
-  private final ObsidianCANSparkMax m_rightFollower = new ObsidianCANSparkMax(Constants.UpperShooterMotorPort);
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
+  public ShootingSubsystem() {
+    
   }
 
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
+  public void setShooterSpeed(double speed){
+    m_rightLeader.set(speed);
+    m_leftFollower.set(-speed);
+  }
+
+  public void stopShooter(){
+    m_rightLeader.set(0);
+    m_leftFollower.set(0);
   }
 
   @Override
