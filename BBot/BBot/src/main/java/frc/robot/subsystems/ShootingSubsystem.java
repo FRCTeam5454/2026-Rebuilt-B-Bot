d0f23d1ed37f0199345a457f5d2326c0af39f24f
Karl Kay <kkay99@gmail.com>
2026-08-17 19:59:05 -0500

    Made ShootSubsys changes but  are undefined meth.

--- BBot/BBot/src/main/java/frc/robot/subsystems/ShootingSubsystem.java
+++ BBot/BBot/src/main/java/frc/robot/subsystems/ShootingSubsystem.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.utilities.ObsidianCANSparkMax;
+
 public class ShootingSubsystem extends SubsystemBase {
    final ObsidianCANSparkMax m_rightLeader = new ObsidianCANSparkMax(Constants.ShooterConstants.LowerShooterMotorPort, ObsidianCANSparkMax.MotorType.kBrushless, false);
    final ObsidianCANSparkMax m_rightFollower = new ObsidianCANSparkMax(Constants.ShooterConstants.UpperShooterMotorPort, ObsidianCANSparkMax.MotorType.kBrushless, false);
@@ -17,15 +18,23 @@ public class ShootingSubsystem extends SubsystemBase {
   
   /** Creates a new ShootingSubsystem. */
   public ShootingSubsystem() {
+    // Link the right follower to do exactly what the right leader does
+    m_rightFollower.follow(m_rightLeader);
     
+    // Link the left follower to the right leader, but invert its direction (true) 
+    // so the left and right wheels spin inward together to shoot
+    m_leftFollower.follow(m_rightLeader);
+    m_leftFollower.setInverted(true);
+
   }
 
   public void setShooterSpeed(double speed){
+    // Because the other motors are following this one, you only need to command the leader
     m_rightLeader.set(speed);
-    m_leftFollower.set(-speed);
   }
 
   public void stopShooter(){
+    // Stopping the leader automatically stops the followers
     m_rightLeader.set(0);
     m_leftFollower.set(0);
   }
