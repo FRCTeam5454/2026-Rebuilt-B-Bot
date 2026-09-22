cd45588e3664dce03474a3467b34ab77409466a4
asherbug <144501278+asherbug@users.noreply.github.com>
2026-09-21 19:14:19 -0500

    Asher's quick fix for auto crash

--- BBot/src/main/java/frc/robot/RobotContainer.java
+++ BBot/src/main/java/frc/robot/RobotContainer.java
@@ -91,8 +91,8 @@ public class RobotContainer {
       configureBindings();
       resetDefaultCommand();
       configureNamedCommands();
-      m_autoChooser=AutoBuilder.buildAutoChooser();
       createAutonomousCommandList();
+      m_autoChooser=AutoBuilder.buildAutoChooser();
   }
 
   private void configureBindings() {
